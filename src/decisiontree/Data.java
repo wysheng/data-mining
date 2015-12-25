72c227f4f419a8cf4c16ebcfb6409de0b8ff83d0
fonxian <fonxian2015@163.com>
2015-12-25 14:41:18 +0800

    DecisionTree feature

--- src/decisiontree/Data.java
+++ src/decisiontree/Data.java
@@ -1,14 +1,16 @@
 package decisiontree;
 
+import java.util.HashMap;
+
 public class Data {
 	private int id;
-	private int feature1;
-	private int feature2;
+	private HashMap<Integer,String> feature;
 	private String type;
-	public Data(int id, int feature1, int feature2, String type) {
+
+	public Data(int id, HashMap<Integer, String> feature, String type) {
+		super();
 		this.id = id;
-		this.feature1 = feature1;
-		this.feature2 = feature2;
+		this.feature = feature;
 		this.type = type;
 	}
 	public int getId() {
@@ -17,17 +19,12 @@ public class Data {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getFeature1() {
-		return feature1;
+	
+	public HashMap<Integer, String> getFeature() {
+		return feature;
 	}
-	public void setFeature1(int feature1) {
-		this.feature1 = feature1;
-	}
-	public int getFeature2() {
-		return feature2;
-	}
-	public void setFeature2(int feature2) {
-		this.feature2 = feature2;
+	public void setFeature(HashMap<Integer, String> feature) {
+		this.feature = feature;
 	}
 	public String getType() {
 		return type;
--- src/decisiontree/DecisionTree.java
+++ src/decisiontree/DecisionTree.java
@@ -2,26 +2,91 @@ package decisiontree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * 计算机决策树的熵
+ * 决策树
+ * 
+ * 1、创建数据集
+ * 2、计算香农熵或信息增益 
+ * 3、通过计算选择最优数据集划分特征 
+ * 4、通过最优数据划分特征来划分数据集，创建决策树
+ * 
  * 
  * @author fzj
  * 
  */
 public class DecisionTree {
+
 	public static void main(String[] args) {
 		// 创建数据集
 		List<Data> dataList = createData();
-		//香农熵
+		// 香农熵
 		double shannonEnt = computeShannonEnt(dataList);
-		System.out.println("香农熵为："+shannonEnt);
+		System.out.println("香农熵为：" + shannonEnt);
+		List<Data> retDataSet = splitDataList(dataList, 1, "0");
+		int t = findBestFeatureToSplit(dataList);
+		System.out.println(t);
 	}
-	
-	//计算数据集的香农熵
-	public static double computeShannonEnt(List<Data> dataList){
+	//分类
+	public static String majorityCnt(ArrayList<String> classList){
+		HashMap<String,Integer> classCount = new HashMap<String,Integer>();
+		for(String vote:classList){
+			if(!classCount.containsKey(vote)){
+				classCount.put(vote, 0);
+			}
+			classCount.put(vote, classCount.get(vote)+1);
+		}
+		return null;
+	}
+	// 选择最优数据集划分特征
+	public static int findBestFeatureToSplit(List<Data> dataList) {
+		int numFeatures = dataList.get(0).getFeature().size()-1;
+		double baseEntroy = computeShannonEnt(dataList);
+		double bestInfoGain = 0.0;
+		int bestFeature = -1;
+		ArrayList<String> featList = new ArrayList<String>();
+		ArrayList<Data> subDataSet = null;
+		for (int i = 0; i < numFeatures; i++) {
+			for (Data data : dataList) {
+				featList.add(data.getFeature().get(i));
+			}
+			Set<String> set = new HashSet(featList);
+			double newEntropy = 0.0;
+			for (String str : set) {
+				subDataSet = splitDataList(dataList, i, str);
+				double size1 = dataList.size();
+				double size2 = subDataSet.size();
+				double prob = size2/ size1;
+				newEntropy += prob * computeShannonEnt(dataList);
+			}
+			double infoGain = baseEntroy - newEntropy;
+			if (infoGain >= bestInfoGain) {
+				bestInfoGain = infoGain;
+				bestFeature = i;
+			}
+			featList.clear();
+		}
+		return bestFeature;
+	}
+
+	// 数据集划分
+	public static ArrayList<Data> splitDataList(List<Data> dataList, int axis,
+			String value) {
+		ArrayList<Data> retDataSet = new ArrayList<Data>();
+		for (Data featVec : dataList) {
+			if (featVec.getFeature().get(axis).equals(value)) {
+				retDataSet.add(featVec);
+			}
+		}
+		return retDataSet;
+	}
+
+	// 计算数据集的香农熵
+	public static double computeShannonEnt(List<Data> dataList) {
 		long num = dataList.size();
 		Map<String, Integer> labelCounts = new HashMap<String, Integer>();
 		String currentLabel = "";
@@ -33,6 +98,7 @@ public class DecisionTree {
 		}
 		double shannonEnt = 0.0;
 		double prob;
+		// 香农熵公式
 		for (Map.Entry<String, Integer> entry : labelCounts.entrySet()) {
 			prob = (double) entry.getValue() / num;
 			shannonEnt -= prob * computeLog(prob, 2);
@@ -43,16 +109,35 @@ public class DecisionTree {
 	// 创建数据集
 	public static ArrayList<Data> createData() {
 		ArrayList<Data> dataList = new ArrayList<Data>();
-		Data data1 = new Data(1, 1, 1, "maybe");
-		Data data2 = new Data(2, 1, 1, "yes");
-		Data data3 = new Data(3, 1, 0, "no");
-		Data data4 = new Data(4, 0, 1, "no");
-		Data data5 = new Data(5, 0, 1, "no");
+		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map2 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map3 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map4 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map5 = new HashMap<Integer, String>();
+
+		map1.put(0, "1");
+		map1.put(1, "1");
+		map2.put(0, "1");
+		map2.put(1, "1");
+		map3.put(0, "1");
+		map3.put(1, "0");
+		map4.put(0, "0");
+		map4.put(1, "1");
+		map5.put(0, "0");
+		map5.put(1, "1");
+
+		Data data1 = new Data(1, map1, "yes");
+		Data data2 = new Data(2, map2, "yes");
+		Data data3 = new Data(3, map3, "no");
+		Data data4 = new Data(4, map4, "no");
+		Data data5 = new Data(5, map5, "no");
+
 		dataList.add(data1);
 		dataList.add(data2);
 		dataList.add(data3);
 		dataList.add(data4);
 		dataList.add(data5);
+
 		return dataList;
 	}
 
